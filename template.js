6e3461ec88610b9fb5ee7d2824471e78442f7bff
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2025-11-25 04:34:37 +0100

    Add utility for routing requests and inferring service origins

---
+++ template.js
@@ -0,0 +1,40 @@
+import {hydrateRoutePathname, inferNetwork, inferRoute, inferRouteService} from '@shakerquiz/utilities'
+
+export const ServiceNetworkOrigin = Object.freeze({/* ... */})
+
+/** @returns {string} */
+export const inferOrigin = Object.freeze((service, network) => {
+    if (!(service in ServiceNetworkOrigin))
+        throw TypeError(`Service '${service}' does not exist.`)
+
+    if (!(network in ServiceNetworkOrigin[service]))
+        throw TypeError(`Network '${network}' in Service '${service}' does not exist.`)
+
+    return ServiceNetworkOrigin[service][network]
+})
+
+export const routeRequest = Object.freeze((maybeNetwork, maybeRoute, maybeRouteParams, maybeRouteSearch, init) => {
+    var route = inferRoute(maybeRoute)
+
+    if (route === 'Unknown')
+        throw TypeError(`[routeRequest] Could not infer route for value: '${maybeRoute}'.`)
+
+    var service = inferRouteService(maybeRoute)
+
+    if (service === 'Unknown')
+        throw TypeError(`[routeRequest] Could not infer service for value: '${maybeRoute}'.`)
+
+    var network = inferNetwork(maybeNetwork)
+
+    if (network === 'Unknown')
+        throw TypeError(`[routeRequest] Could not infer network for value: '${maybeRoute}'.`)
+
+    var url = new URL(
+        hydrateRoutePathname(route, maybeRouteParams),
+        inferOrigin(service, network),
+    )
+
+    url.search = maybeRouteSearch
+
+    return fetch(url, init)
+})
